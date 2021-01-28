c7865d7d917a79f0946127f440d37dd17920de0c
locnx-iotvn <=>
2021-01-28 15:40:31 +0700

    update finish

--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -1,4 +1,11 @@
-import { ADD_TODO, DELETE_TODO, EDIT_TODO, COMPLETE_TODO, CLEAR_COMPLETED } from '../constants/ActionTypes';
+import {
+  ADD_TODO,
+  DELETE_TODO,
+  EDIT_TODO,
+  COMPLETE_TODO,
+  COMPLETE_ALL,
+  CLEAR_COMPLETED
+} from '../constants/ActionTypes';
 
 const initialState = [
   {
@@ -33,8 +40,15 @@ export default function todos(state = initialState, action) {
         todo => todo.id === action.id ? { ...todo, completed: !todo.completed } : todo,
       );
 
-      case CLEAR_COMPLETED:
-        return state.filter(todo => todo.completed === false);
+    case COMPLETE_ALL:
+      const areAllMarked = state.every(todo => todo.completed);
+      return state.map(todo => ({
+        ...todo,
+        completed: !areAllMarked
+      }));
+
+    case CLEAR_COMPLETED:
+      return state.filter(todo => todo.completed === false);
 
     default:
       return state;
